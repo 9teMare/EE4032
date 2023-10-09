8fb70d66e70431299ba5db2c47d1253c223dd77f
9teMare <wang.zihao@u.nus.edu>
2023-10-09 19:54:46 +0800

    remove search box

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -12,9 +12,6 @@ export default function Navbar() {
                 <a className="btn btn-ghost normal-case text-xl">EE4032 Blockchain Engineering</a>
             </div>
             <div className="flex-none gap-2">
-                <div className="form-control">
-                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
-                </div>
                 {metamask && address ? (
                     <div className="dropdown dropdown-end">
                         <label tabIndex={0} className="btn btn-ghost btn-circle">
