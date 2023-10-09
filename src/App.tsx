59ea190ec87d097992adf76f1dab20330b8d5e56
9teMare <wang.zihao@u.nus.edu>
2023-10-09 17:37:49 +0800

    connect metamask

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,75 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Web3 from "web3";
+import { ethers } from "ethers";
 
 function App() {
-    const [count, setCount] = useState(0);
+    const [haveMetamask, setHaveMetamask] = useState(false);
+    const [ethereum, setEthereum] = useState<any>(null); // window.ethereum
+    const [network, setNetwork] = useState<string | null>(null);
+    const [isConnected, setIsConnected] = useState(false);
+    const [balance, setBalance] = useState<string | null>(null);
+    const [address, setAddress] = useState(null);
+
+    const provider = new ethers.BrowserProvider(ethereum);
+
+    const isMetaMaskInstalled = () => {
+        //@ts-ignore
+        const { ethereum } = window;
+        setHaveMetamask(ethereum);
+        if (ethereum) {
+            setEthereum(ethereum);
+        }
+    };
+
+    useEffect(() => {
+        isMetaMaskInstalled();
+    }, []);
+
+    const connectWallet = async () => {
+        try {
+            const accounts = await ethereum.request({
+                method: "eth_requestAccounts",
+            });
+
+            const chainId = await ethereum.request({
+                method: "eth_chainId",
+            });
+
+            let balanceVal: ethers.BigNumberish = await provider.getBalance(accounts[0]);
+            console.log(balanceVal);
+
+            //@ts-ignore
+            let bal = ethers.formatEther(balanceVal);
+
+            console.log(bal);
+            if (chainId === "0x3") {
+                setNetwork("Ropsten Test Network");
+            } else if (chainId === "0x5") {
+                setNetwork("Goerli Test Network");
+            } else if (chainId === "0xaa36a7") {
+                setNetwork("Sepolia Test Network");
+            } else {
+                setNetwork("Other Test Network");
+            }
+            setAddress(accounts[0]);
+            setBalance(bal);
+            setIsConnected(true);
+
+            // navigate('/InterfaceDemo/profile');
+        } catch (error) {
+            setIsConnected(false);
+            console.log(error);
+        }
+    };
 
     return (
         <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            <button onClick={async () => await connectWallet()}>Connect</button>
+            {haveMetamask ? <div>Have Metamask</div> : <div>Don't Have Metamask</div>}
+            {isConnected ? <div>Connected Metamask</div> : <div>Not Connected Metamask</div>}
         </>
     );
 }
